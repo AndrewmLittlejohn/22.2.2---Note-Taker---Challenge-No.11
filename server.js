3285c5e6c64d5b4c947d54f293b9bd9210ca6195
Andrew Littlejohn <andrewlittlejohn@Andrews-MacBook-Air.local>
2023-02-01 16:41:19 -0500

    afternoon commit

--- server.js
+++ server.js
@@ -10,22 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/assets/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 /* #region main - GET Request used to show details of previously created requests*/
 // GET request for reviews
-// app.get('/api/notes', (req, res) => {
-//   // Send a message to the client
-//   res.json(`${req.method} request received to get reviews`);
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.json(`${req.method} request received to get reviews`);
 
-//   // Log our request to the terminal
-//   console.info(`${req.method} request received to get reviews`);
-// });
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
+});
 /* #endregion */ 
 
 // POST Request to add review
@@ -44,32 +45,24 @@ app.post('/api/notes', (req, res) => {
       text,
     };
 
-    // Convert the data to a string so we can save it
-    const reviewNote = JSON.stringify(newNote);
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  if (err){ 
+    console.log(err)
+  } else {
 
+    const parsedNotes = JSON.parse(data);
 
-  /* #region Main - this is to write to a file, REMOVE BEFORE FINAL */  
-    // Write the string to a file
-    fs.writeFile(`./db/${newNote.title}.json`, reviewNote, (err) =>
+    parsedNotes.push(newNote);
+  
+
+  fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (err) =>
       err
         ? console.error(err)
         : console.log(
-            `Review for "${newNote.title}" has been written to JSON file`
-          )
+            `Review for "${newNote.title}" has been added`)
     );
-  /* #endregion */
-
-  fs.writeFile(`./db/${newNote.title}.json`, reviewNote, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for "${newNote.title}" has been written to JSON file`
-          )
-    );
-
-
-
-
+  }
+});
     const Note = {
       status: 'success',
       body: newNote,
@@ -79,8 +72,10 @@ app.post('/api/notes', (req, res) => {
     res.status(201).json(Note);
   } else {
     res.status(500).json('Error - note not posted, please try again or contact IT Help Desk for for additional support');
-  }
+  };
 });
+
+  
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
